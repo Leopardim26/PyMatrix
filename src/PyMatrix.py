f60e2108b7c227bc3fce709ec3523207ef1e082e
Leopardim26 <117026317+Leopardim26@users.noreply.github.com>
2024-03-17 12:35:56 -0300

    Biblioteca de matriz final
    Bem joia

--- src/PyMatrix.py
+++ src/PyMatrix.py
@@ -1,71 +1,137 @@
 class Matrix:
-    def __init__(self,rows, cols):
-        self.row_ = rows
-        self.col_ = cols
+    def __init__(self, lines, columns):
+        self.row = lines
+        self.col = columns
+        self.matrix = [[0 for _ in range(columns)] for _ in range(lines)]
 
-        for i in range(self.row_):
-            for o in range(self.col_):
-                self.matrix_[i][o] = 0
+    def allocate(self):
+        if self.row != 0 and self.col != 0:
+            self.matrix = [[0 for _ in range(self.col)] for _ in range(self.row)]
 
-    def get(self,row,col):
-        return self.matrix_[row][col]
+    def deallocate(self):
+        self.matrix = []
 
-    def set(self,row,col,value):
-        if self.row_ == row and self.col_ == col:
-            self.matrix_[row][col] = value
+    def initialize(self):
+        self.matrix = [[0 for _ in range(self.col)] for _ in range(self.row)]
 
-    def __add__(self, second):
-        if self.row_ == second.row() and self.col_ == second.col():
-            for i in range(self.row_):
-                for o in range(self.col_):
-                    set(i,o, self.matrix_[i][o]+second.get(i,o))
+    def set_size(self, lines, columns):
+        self.deallocate()
+        self.row = lines
+        self.col = columns
+        self.allocate()
+        self.initialize()
 
-    def __sub__(self, second):
-        if self.row_ == second.row() and self.col_ == second.col():
-            for i in range(self.row_):
-                for o in range(self.col_):
-                    set(i,o, self.matrix_[i][o]-second.get(i,o))
+    def get(self, i, j):
+        return self.matrix[i][j]
 
-    def __mul__(self, second):
-        if self.col_ == second.row():
-            temp = Matrix(self.row_, second.col())
-            for i in range(self.row_):
-                for o in range(second.col()):
-                    for e in range(second.row()):
-                        temp.set(i,o, temp.get(i,o)+self.matrix_[i][e]*second.get(e,o))
+    def set(self, i, j, value):
+        self.matrix[i][j] = value
+
+    def copy_from(self, other):
+        assert self.row == other.row and self.col == other.col
+        self.matrix = [row[:] for row in other.matrix]
+
+    def print_matrix(self):
+        for row in self.matrix:
+            print(" ".join(map(str, row)))
+        print("--------")
+
+    def identity(size):
+        return Matrix.diag(size, 1)
+
+    def diag(size, diag_value):
+        M = Matrix(size, size)
+        for i in range(size):
+            M.set(i, i, diag_value)
+        return M
+
+    def tran(self):
+        N = Matrix(self.col, self.row)
+        for i in range(self.row):
+            for e in range(self.col):
+                N.set(e, i, self.matrix[i][e])
+        return N
+
+    def inv(self):
+        N = Matrix(self.col, self.row)
+        if self.row == 1 and self.col == 1:
+            N.set(0, 0, 1 / self.get(0, 0))
+        elif self.row == 2 and self.col == 2:
+            det = self.get(0, 0) * self.get(1, 1) - self.get(1, 0) * self.get(0, 1)
+            N.set(0, 0, self.get(0, 0) / det)
+            N.set(0, 1, -self.get(0, 1) / det)
+            N.set(1, 1, self.get(1, 1) / det)
+            N.set(1, 0, -self.get(1, 0) / det)
+        return N
+
+    def __add__(self, other):
+        assert self.row == other.row and self.col == other.col
+        temp = Matrix(self.row, self.col)
+        for i in range(self.row):
+            for j in range(self.col):
+                temp.set(i, j, self.matrix[i][j] + other.get(i, j))
+        return temp
+
+    def __sub__(self, other):
+        assert self.row == other.row and self.col == other.col
+        temp = Matrix(self.row, self.col)
+        for i in range(self.row):
+            for j in range(self.col):
+                temp.set(i, j, self.matrix[i][j] - other.get(i, j))
+        return temp
+
+    def __radd__(self, k):
+        temp = Matrix(self.row, self.col)
+        for i in range(self.row):
+            for j in range(self.col):
+                temp.set(i, j, self.matrix[i][j] + k)
+        return temp
+
+    def __rsub__(self, k):
+        temp = Matrix(self.row, self.col)
+        for i in range(self.row):
+            for j in range(self.col):
+                temp.set(i, j, k - self.matrix[i][j])
+        return temp
+
+    def __mul__(self, other):
+        if isinstance(other, Matrix):
+            assert self.col == other.row
+            temp = Matrix(self.row, other.col)
+            for i in range(self.row):
+                for j in range(other.col):
+                    temp.set(i, j, sum(self.matrix[i][k] * other.get(k, j) for k in range(self.col)))
+            return temp
+        else:
+            temp = Matrix(self.row, self.col)
+            for i in range(self.row):
+                for j in range(self.col):
+                    temp.set(i, j, self.matrix[i][j] * other)
             return temp
 
-    def __eq__(self,second):
-        if self.row_ == second.row() and self.col_ == second.col():
-            for i in range(self.row_):
-                for o in range(self.col_):
-                    set(i,o, second.get(i,o))
+    def __rmul__(self, k):
+        temp = Matrix(self.row, self.col)
+        for i in range(self.row):
+            for j in range(self.col):
+                temp.set(i, j, self.matrix[i][j] * k)
+        return temp
 
-    def t(self):
-        temp = Matrix(self.col_,self.row_)
-        for i in range(self.row_):
-            for o in range(self.col_):
-                temp.set(o,i, self.matrix_[i][o])
+    def __iadd__(self, other):
+        assert self.row == other.row and self.col == other.col
+        for i in range(self.row):
+            for j in range(self.col):
+                self.matrix[i][j] += other.get(i, j)
+        return self
 
-    def i(self):
-        temp = Matrix(self.row_, self.col_)
-        if self.row_ == 1 and self.col_ == 1:
-            self.matrix_[0][0] = self.matrix_[0][0]*-1
-        elif self.row_ == 2 and self.col_ == 2:
-            det = self.get(0,0) * self.get(1,1) - self.get(1,0) * self.get(0,1)
-            temp.set(0,0, self.get(0,0)/det)
-            temp.set(0,1, -self.get(0,1)/det)
-            temp.set(1,1, self.get(1,1)/det)
-            temp.set(1,0, -self.get(1,0)/det)
+    def __isub__(self, other):
+        assert self.row == other.row and self.col == other.col
+        for i in range(self.row):
+            for j in range(self.col):
+                self.matrix[i][j] -= other.get(i, j)
+        return self
 
-    def print(self):
-        for i in range(self.row_):
-            for o in range(self.col_):
-                t+= str(self.matrix_[o][i])
-            print(t)
-
-    def row(self):
-        return self.row_
-    
-    def col(self):
-        return self.col_
+    def __imul__(self, k):
+        for i in range(self.row):
+            for j in range(self.col):
+                self.matrix[i][j] *= k
+        return self
